98fc574ad8ae4e6f3fa29671e8793c8b5d8a4bce
cruetto <131352364+cruetto@users.noreply.github.com>
2024-01-15 16:21:35 +0000

    Smartphone Navbar created

--- main.js
+++ main.js
@@ -20,6 +20,15 @@ function copyTextToClipboard(textToCopy, nameOfObject) {
 	// });
 }
 
+function HideNavbar() {
+	var x = document.getElementById("navbar");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+ }
+
 
 
 
